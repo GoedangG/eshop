bf911534e41a7722b5e37095d699f705aa4be22e
Bayu <rizqi.bayu@ui.ac.id>
2024-03-09 23:55:20 +0700

    [RED] Add PaymentServiceImplTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class OrderServiceTest {
+class OrderServiceImplTest {
     @InjectMocks
     OrderServiceImpl orderService;
     @Mock

80e6095b1e7fe1bd2a20126a0209ea8768616fb5
Bayu <rizqi.bayu@ui.ac.id>
2024-03-10 18:50:47 +0700

    [RED] create PaymentVoucherTest.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherTest.java
@@ -0,0 +1,41 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import enums.PaymentStatus;
+import enums.PaymentMethod;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PaymentVoucherTest {
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp(){
+        this.paymentData = new HashMap<>();
+    }
+
+    @Test
+    void testSetPaymentDataEmpty(){
+        PaymentVoucherCode payment = new PaymentVoucherCode("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.VOUCHER_CODE.getValue(), this.paymentData);
+        assertThrows(IllegalArgumentException.class, () -> payment.setPaymentData(this.paymentData));
+    }
+
+    @Test
+    void testSetPaymentDataValid(){
+        this.paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        PaymentVoucherCode payment = new PaymentVoucherCode("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.VOUCHER_CODE.getValue(), this.paymentData);
+        payment.setPaymentData(this.paymentData);
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testSetPaymentDataInvalidVoucherCode(){
+        this.paymentData.put("voucherCode", "towewew");
+        PaymentVoucherCode payment = new PaymentVoucherCode("13652556-012a-4c07-b546-54eb1396d79b", PaymentMethod.VOUCHER_CODE.getValue(), this.paymentData);
+        payment.setPaymentdata(this.paymentData);
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
+    }
+}
